2f2b1a84c3560fd54f0a428ac6049bb59c9835ed
Mohammed Shabbir ALI <111312633+msa-1988@users.noreply.github.com>
2023-04-26 07:49:39 +0200

    Update DiabetesPredictionWebApp.py

--- DiabetesPredictionWebApp.py
+++ DiabetesPredictionWebApp.py
@@ -54,7 +54,7 @@ def main():
     BloodPressure = st.number_input('Blood Pressure', min_value=0, max_value=200, step=1, value=0)
     SkinThickness = st.number_input('Skin Thickness', min_value=0, max_value=100, step=1, value=0)
     Insulin = st.number_input('Insulin Level', min_value=0, max_value=1000, step=1, value=0)
-    BMI = st.number_input('BMI', min_value=0, max_value=60, step=0.1, value=0.0)
+    BMI = st.number_input('BMI', min_value=0, max_value=60, step=1, value=0)
     DiabetesPedigreeFunction = st.number_input('Diabetes Pedigree Function', min_value=0.0, max_value=5.0, step=0.1, value=0.0)
     Age = st.number_input('Age', min_value=1, max_value=120, step=1, value=1)
 
